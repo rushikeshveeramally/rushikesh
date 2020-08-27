153708d74603e5b4536c651d7234629cb8a485a4
rushikesh veeramally <43981800+rushikeshveeramally@users.noreply.github.com>
2020-08-28 03:54:14 +0530

    Updates

---
+++ precache-manifest.cf0abe43d5f5aef132df4ff10ad435ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a048d5d22da4831d0b7f36fa72e98fef",
+    "url": "/rushikesh/index.html"
+  },
+  {
+    "revision": "a7c786cc5363f7c51e3d",
+    "url": "/rushikesh/static/css/main.d4d62046.chunk.css"
+  },
+  {
+    "revision": "8288a96bcd522619a9f8",
+    "url": "/rushikesh/static/js/2.44bb0385.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rushikesh/static/js/2.44bb0385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c786cc5363f7c51e3d",
+    "url": "/rushikesh/static/js/main.2a8e135c.chunk.js"
+  },
+  {
+    "revision": "7b52e7983fb4d260f862",
+    "url": "/rushikesh/static/js/runtime-main.a2b2ab5b.js"
+  },
+  {
+    "revision": "f636c6b7dbf5ef324a35971e13d7776a",
+    "url": "/rushikesh/static/media/img-1.f636c6b7.png"
+  },
+  {
+    "revision": "031f24e003646c264dd57a08324442f6",
+    "url": "/rushikesh/static/media/jeshoots-com-pUAM5hPaCRI-unsplash.031f24e0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rushikesh/static/media/logo.5d5d9eef.svg"
+  }
+]);
